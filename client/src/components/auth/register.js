3a2b48d3864e1ef9233899e0210b3fb60c4cb628
Arun <arunnair018@gmail.com>
2021-06-13 19:31:53 +0530

    updated for heroku

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -22,16 +22,17 @@ class Register extends Component {
   // function to submit the form
   handleSubmit = (e) => {
     e.preventDefault(); // prevents default working of function
-    console.log(this.state.role)
+    console.log(this.state.role);
     // form the body of request
 
     if (
-      this.state.role==='Mentor' && MD5(this.state.secret).toString() !== "63bcc58aa5ba755987c3bb16e495dd2d"
+      this.state.role === "Mentor" &&
+      MD5(this.state.secret).toString() !== "63bcc58aa5ba755987c3bb16e495dd2d"
     ) {
       this.setState({
         errors: "Invalid Secret...",
       });
-    } 
+    }
     const data = {
       username: this.state.username,
       role: this.state.role,
@@ -46,8 +47,8 @@ class Register extends Component {
       .catch((err) => {
         // API call not succeed set error to display
         this.setState({ errors: "error submitting" });
-  });
-}
+      });
+  };
 
   // check for password matching
   confirmPassword = (e) => {
@@ -65,68 +66,71 @@ class Register extends Component {
     const errors = this.state.errors;
     return (
       <div>
-        <div className='container'>
+        <div className="container">
           <form onSubmit={this.handleSubmit}>
             <h3>Register</h3>
             <br />
-            <div className='form-group'>
+            <div className="form-group">
               <input
-                type='text'
-                className='form-control'
-                placeholder='Enter username'
-                id='username'
+                type="text"
+                className="form-control"
+                placeholder="Enter username"
+                id="username"
                 onChange={this.handleChange}
                 required
               />
             </div>
 
-            <div className='form-group'>
+            <div className="form-group">
               <input
-                type='password'
-                className='form-control'
-                placeholder='Enter password'
-                id='password'
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                id="password"
                 onChange={this.handleChange}
                 required
               />
             </div>
-            <div className='form-group'>
+            <div className="form-group">
               <input
-                type='password'
-                className='form-control'
-                placeholder='Confirm password'
-                id='confirm_password'
+                type="password"
+                className="form-control"
+                placeholder="Confirm password"
+                id="confirm_password"
                 onChange={this.confirmPassword}
                 required
               />
             </div>
-            <div className='form-group'>
-              <input
-                type='password'
-                className='form-control'
-                placeholder='Enter secret'
-                id='secret'
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className='form-group'>
+
+            <div className="form-group">
               <select
-                class='browser-default select'
-                id='role'
-                defaultValue='0'
+                class="browser-default select"
+                id="role"
+                defaultValue="0"
                 onChange={this.handleChange}
               >
-                <option value='student'>Student</option>
-                <option value='mentor'>Mentor</option>
+                <option value="student">Student</option>
+                <option value="mentor">Mentor</option>
               </select>
             </div>
+            {this.state.role !== "student" && (
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter secret"
+                  id="secret"
+                  onChange={this.handleChange}
+                />
+              </div>
+            )}
             <br />
-            <button type='submit' className='btn btn-primary'>
+            <button type="submit" className="btn btn-primary">
               Submit
             </button>
             {errors ? (
               <center>
-                <span className='error'>{errors}</span>
+                <span className="error">{errors}</span>
               </center>
             ) : (
               ""

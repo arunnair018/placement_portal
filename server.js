b11acbcb136cc6f69ca36364aba2de9e37898311
Arun <arunnair018@gmail.com>
2021-06-14 05:56:03 +0530

    minor update

--- server.js
+++ server.js
@@ -26,11 +26,9 @@ db.connect();
 
 // Serve static files from the React app
 
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 routes(app);
 
 //server listening on localhost port 3000
